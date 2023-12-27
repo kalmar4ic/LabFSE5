614fb8271a8b699baeeb54332a34f348d79b9ddf
kalmar4ic <serjcherkashin228@gmail.com>
2023-12-27 23:22:45 +0700

    Initial Commit

---
+++ Lab5-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab5-6
+//
+//  Created by Сергей Черкашин on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
